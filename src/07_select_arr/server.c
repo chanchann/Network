b6c357a0f9021624c34ca960727f38587e5d5181
chanchann <www.769440972@qq.com>
2020-11-07 22:05:42 +0800

    select

--- src/07_select_arr/server.c
+++ src/07_select_arr/server.c
@@ -4,10 +4,12 @@
 
 int main(int argc, char** argv)
 {
-    int i, j, n, nready;
-    int maxfd = 0;
+    int i, j, n, nready, maxi;
+    // 自定义数组client,防止遍历1024个文件描述符 FD_SETSIZE默认1024
+    int client[FD_SETSIZE];
+    int maxfd = 0, sockfd;
     int lfd = 0, cfd = 0; 
-    char buf[BUFSIZ]; // BUFSIZ : 1024
+    char buf[BUFSIZ], str[INET_ADDRSTRLEN];  // #define INET_ADDRSTRLEN 16
     struct sockaddr_in serv_addr, clit_addr;
     socklen_t clit_addr_len;
     
@@ -29,9 +31,15 @@ int main(int argc, char** argv)
     // rset读事件文件描述符集合，allset用来暂存
     fd_set rset, allset;
     maxfd = lfd;
-
     FD_ZERO(&allset);
     FD_SET(lfd, &allset);
+
+    maxi = -1; // 将来作为client[]的下标，初始值指向0个元素之前下标位置
+    // client[]全部初始化为-1
+    for(i = 0; i < FD_SETSIZE; i++) {
+        client[i] = -1;
+    }
+
     while(1) {
         // 每次循环都重新设置select监控信号集
         rset = allset;
@@ -43,11 +51,31 @@ int main(int argc, char** argv)
             clit_addr_len = sizeof(clit_addr);
             // Accept 不会阻塞
             cfd = Accept(lfd, (struct sockaddr*)&clit_addr, &clit_addr_len);
+            printf("received from %s at PORT %d\n",
+            inet_ntop(AF_INET, &clit_addr.sin_addr, str, sizeof(str)),
+            ntohs(clit_addr.sin_port));
+            for(i = 0; i < FD_SETSIZE; i++) {  //找client[]中没有使用的位置
+                if(client[i] < 0) {
+                    client[i] = cfd;  // 保存accept返回的文件描述符到client[]里
+                    break;
+                }
+            }
+            // 达到select能监控的文件个数上限1024
+            if(i == FD_SETSIZE) {
+                printf("Too many clients\n");
+                exit(1);
+            }
+            
             //向监控文件描述符集合allset添加新的文件描述符connfd
             FD_SET(cfd, &allset);
             if(maxfd < cfd) {
                 maxfd = cfd;
             }
+            // 保证maxi存的总是client[]最后一个元素下标
+            if(maxi < i) {
+                maxi = i;
+            }
+
             if(nready == 1) { // 只有lfd有事件，后续的for不执行
                 continue;
             }
