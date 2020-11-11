e883ff626b71db4fd2bd401ba34abe6c64d43a55
chanchann <www.769440972@qq.com>
2020-11-11 20:21:31 +0800

    threadpool

--- src/12_thread_pool/threadpool.c
+++ src/12_thread_pool/threadpool.c
@@ -46,8 +46,73 @@ struct threadpool_t {
     int shutdown;       // 标志位，线程池使用状，true or false
 };
 
+void *threadpool_thread(void *threadpool);
+
+void *adjust_thread(void* threadpool);
+
+int is_thread_alive(pthread_t tid);
+
+int threadpool_free(threadpool_t *pool);
+
+// threadpool_create(3, 100, 100);
+threadpool_t *threadpool_create(int min_thr_num, int max_thr_num, int queue_max_size) {
+    int i;
+    threadpool_t *pool = NULL; // 线程池 结构体
+    do {  // 顶替goto，这个不是循环作用，某个地方出错就break跳出
+        if((pool = (threadpool_t*)malloc(sizeof(threadpool_t))) == NULL) {
+            printf("malloc threadpool fail");
+            break;   // 跳出do while
+        }
+        pool->min_thr_num = min_thr_num;
+        pool->max_thr_num = max_thr_num;
+        pool->busy_thr_num =0;
+        pool->live_thr_num = min_thr_num;   // 活着的线程数，初值 - 最小线程数
+        pool->wait_exit_thr_num = 0;
+        pool->queue_size = 0;      // 有0个产品
+        pool->queue_max_size = queue_max_size;      // 最大任务队列数
+        pool->queue_front = 0;
+        pool->queue_rear = 0;
+        pool->shutdown = false;     // 不关闭线程池
+
+        // 根据最大线程上限数，给工作线程数组开辟空间，并清零
+        pool->threads = (pthread_t*)malloc(sizeof(pthread_t) * max_thr_num);
+        if(pool->threads == NULL) {
+            printf("malloc threads fail");
+            break;
+        }
+        memset(pool->threads, 0, sizeof(pthread_t) * max_thr_num);
+        
+        // 给任务队列开辟空间
+        pool->task_queue = (threadpool_task_t *)malloc(sizeof(threadpool_task_t) * queue_max_size);
+        if(pool->task_queue == NULL) {
+            printf("malloc task_queue fail");
+            break;
+        }
+        // 初始化互斥锁，条件变量
+        if(pthread_mutex_init(&(pool->lock), NULL) != 0
+            || pthread_mutex_init(&(pool->thread_counter), NULL) != 0
+            || pthread_cond_init(&(pool->queue_not_empty), NULL) != 0
+            || pthread_cond_init(&(pool->queue_not_full), NULL) != 0) {
+                printf("init the lock or cond fail");
+                break;
+        }
+
+        // 启动min_thr_num个work thread
+        for(i = 0; i < min_thr_num; i++) {
+            pthread_create(&(pool->threads[i]), NULL, threadpool_thread, (void*)pool);  // pool 指向当前线程池
+            printf("start thread 0x%x...\n", (unsigned int)pool->threads[i]);
+        } 
+        // 创建管理者线程
+        pthread_create(&(pool->adjust_tid), NULL, adjust_thread, (void*)pool);
+        return pool;
+    } while(0);
+    threadpool_free(pool);      // 前面代码调用失败，释放poll存储空间
+    return NULL;
+}
+
 int main(int argc, char* argv[]) {
 
+    // threadpool_t *threadpool_create(int min_thr_num, int max_thr_num, int queue_max_size); 
     // 创建线程池，池里最小3个线程，最大100，队列最大100
     threadpool_t *thp = threadpool_create(3, 100, 100); 
     printf("pool init");
